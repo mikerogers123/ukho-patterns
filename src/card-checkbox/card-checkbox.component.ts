701fc7c682e82b702f1f749e3d3f75f4b500f391
Michael Rogers <Michael.Rogers@ukho.gov.uk>
2020-06-26 14:25:50 +0100

    blank checkbox card component

---
+++ src/card-checkbox/card-checkbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-checkbox',
+  templateUrl: './card-checkbox.component.html',
+  styleUrls: ['./card-checkbox.component.scss']
+})
+export class CardCheckboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
