a04b08940334c045b1b1c4ed7ccd94db883e526e
Michael Rogers <Michael.Rogers@ukho.gov.uk>
2020-07-01 08:32:36 +0100

    implement ControlValueAccessor to enable interaction with Angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardCheckboxComponent } from 'src/card-checkbox/card-checkbox.component';
 import { ItemSelectionComponent } from 'src/item-selection/item-selection.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ItemSelectionComponent } from 'src/item-selection/item-selection.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/card-checkbox/card-checkbox.component.ts
+++ src/card-checkbox/card-checkbox.component.ts
@@ -1,17 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Optional, Self } from '@angular/core';
+import { NgControl, ControlValueAccessor } from '@angular/forms';
+
+let nextId = 0;
 
 @Component({
   selector: 'app-card-checkbox',
   templateUrl: './card-checkbox.component.html',
   styleUrls: ['./card-checkbox.component.scss']
 })
-export class CardCheckboxComponent implements OnInit {
+export class CardCheckboxComponent implements ControlValueAccessor {
+  checkboxId = `ukho-radio-${++nextId}`;
 
-  id = Date.now();
+  @Input() disabled = false;
 
-  constructor() { }
+  @Input() validationMessages: Record<string, string> = { required: 'This field is required' };
 
-  ngOnInit(): void {
+  /**
+   * The registered callback function called when an input event occurs on the input element.
+   */
+  public onChange = (value: unknown) => {};
+
+  /**
+   * The registered callback function called when a blur event occurs on the input element.
+   */
+  public onTouch = () => {};
+
+  constructor(@Optional() @Self() protected readonly controlDirective?: NgControl) {
+    if (controlDirective) {
+      controlDirective.valueAccessor = this;
+    }
   }
 
+  get valid() {
+    return this.controlDirective && this.controlDirective.control.valid;
+  }
+
+  get touched() {
+    return this.controlDirective && this.controlDirective.control.touched;
+  }
+
+  get error() {
+    if (!(this.controlDirective && this.controlDirective.errors)) {
+      return null;
+    }
+
+    const errorKeys = Object.keys(this.controlDirective.errors);
+    const activeErrors = errorKeys.filter(errorKey => !!this.controlDirective.errors[errorKey]);
+    return activeErrors[0];
+  }
+  /**
+   * Registers a function called when the control value changes.
+   * Used by ReactiveForms to check for changes
+   */
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(value: unknown): void {
+    this.onChange(value);
+
+    if (this.controlDirective && this.controlDirective.control) {
+      this.controlDirective.control.updateValueAndValidity();
+    }
+  }
 }
